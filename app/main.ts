fa7b5d4387a3def1b2e358dc56b471318114ffd5
Madison <madisonkanna@gmail.com>
2024-07-11 20:51:21 -0700

    clean up code

--- app/main.ts
+++ app/main.ts
@@ -1,17 +1,12 @@
 import * as net from "net";
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
-
-//Uncomment this to pass the first stage
-const server = net.createServer((socket) => {
-	console.log('socket:', socket);
+import { Socket } from "net";
+const server = net.createServer((socket: Socket) => {
 	socket.write('HTTP/1.1 200 OK\r\n\r\nzz');
 	socket.on("close", () => {
 		socket.end();
-
-	});
+	})
 });
 
 server.listen(4221, "localhost");
-
 
