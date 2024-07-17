b34c4940d39ee1ec955e043c11904ebccb56deeb
Madison <madisonkanna@gmail.com>
2024-07-17 08:39:35 -0700

    extract url path

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,31 @@
 import * as net from "net";
+
 console.log("Logs from your program will appear here!");
 import { Socket } from "net";
 const server = net.createServer((socket: Socket) => {
-	socket.write('HTTP/1.1 200 OK\r\n\r\nzz');
 	socket.on("close", () => {
 		socket.end();
 	})
+
+	socket.on('data', (data) => {
+		const req = data.toString();
+		const path = req.split(' ')[1];
+		console.log(path, 'path!!');
+		let res;
+		if (path === '/') {
+
+			console.log(path, 'path!!');
+			res = 'HTTP/1.1 200 OK\r\n\r\n';
+		} else {
+			res = 'HTTP/1.1 404 Not Found\r\n\r\n';
+		}
+		console.log(res, 'res');
+		socket.write(res);
+		socket.end();
+	});
+
+
+
 });
 
 server.listen(4221, "localhost");
